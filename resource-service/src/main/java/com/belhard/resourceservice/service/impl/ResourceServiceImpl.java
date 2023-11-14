ab4ad27ac08dab756fa8623b1610283512ba93e0
Andrei Pronsky <andrey.pronsky@gmail.com>
2023-11-14 10:14:52 +0300

    added songClient invocation

--- resource-service/src/main/java/com/belhard/resourceservice/service/impl/ResourceServiceImpl.java
+++ resource-service/src/main/java/com/belhard/resourceservice/service/impl/ResourceServiceImpl.java
@@ -1,5 +1,6 @@
 package com.belhard.resourceservice.service.impl;
 
+import com.belhard.resourceservice.client.SongClient;
 import com.belhard.resourceservice.data.ResourceRepository;
 import com.belhard.resourceservice.data.entities.Resource;
 import com.belhard.resourceservice.service.MetaDataService;
@@ -8,29 +9,28 @@ import com.belhard.resourceservice.service.ResourceService;
 import com.belhard.resourceservice.service.dto.MetaDataDto;
 import com.belhard.resourceservice.service.dto.ResourceIdDto;
 import com.belhard.resourceservice.service.dto.ResourceIdsDto;
-//import com.belhard.songservice.service.SongService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ResourceServiceImpl implements ResourceService {
 
     private final ResourceRepository resourceRepository;
     private final ResourceMapper mapper;
-
     private final MetaDataService metaDataService;
-
-//    private final SongService songService;
+    private final SongClient songClient;
 
     @Override
     public ResourceIdDto upload(byte[] data) {
         Resource resource = resourceRepository.save(mapper.toEntity(data));
         MetaDataDto metaDataDto = metaDataService.getMetaData(data);
         metaDataDto.setResourceId(resource.getId());
-//        songService.save(metaDataDto);
+        songClient.create(metaDataDto);
         return mapper.toIdDto(resource);
     }
 
@@ -44,6 +44,7 @@ public class ResourceServiceImpl implements ResourceService {
     @Override
     public ResourceIdsDto delete(List<Long> ids) {
         resourceRepository.deleteAllById(ids);
+        songClient.deleteByIds(ids);
         return mapper.toDto(ids);
     }
 }
